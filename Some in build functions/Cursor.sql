7e713c4d47d8b4626d29302ae7d172fc6a7800f4
Vishal <vishalpal1501@gmail.com>
2024-06-28 11:01:45 +0530

    Cursor with variable

--- Some in build functions/Cursor.sql
+++ Some in build functions/Cursor.sql
@@ -1,3 +1,4 @@
+--Without cursor variable
 declare myCursor cursor scroll for 
 select * from employees
 open myCursor
@@ -14,3 +15,24 @@ open myCursor
 fetch absolute 4 from myCursor
 close myCursor
 deallocate  myCursor
+
+
+--With cursor variable
+declare myCursor cursor scroll for 
+select id,name from employees
+declare @emp_id int, @emp_name varchar(50)
+open myCursor
+fetch first from myCursor into @emp_id,@emp_name
+print 'Employee is : ' + cast(@emp_id as varchar) + ' '+ @emp_name
+fetch next from myCursor into @emp_id,@emp_name
+print 'Employee is : ' + cast(@emp_id as varchar) + ' '+ @emp_name
+fetch last from myCursor into @emp_id,@emp_name
+print 'Employee is : ' + cast(@emp_id as varchar) +' '+  @emp_name
+fetch prior from myCursor into @emp_id,@emp_name
+print 'Employee is : ' + cast(@emp_id as varchar) + ' ' + @emp_name
+fetch absolute 4 from myCursor into @emp_id,@emp_name
+print 'Employee is : ' + cast(@emp_id as varchar) +' '+ @emp_name
+fetch relative -1 from myCursor into @emp_id,@emp_name
+print 'Employee is : ' + cast(@emp_id as varchar) +' '+ @emp_name
+close myCursor
+deallocate  myCursor;
